647ee7880334ca472ec5f591a132e0d2207f990d
Ploika <hoofsley@i.ua>
2021-06-29 20:11:50 +0300

    lesson3

---
+++ lesson3/middlewares/user.middleware.js
@@ -0,0 +1,25 @@
+const userService = require('../services/user.service');
+const userRolesEnum = require('../constans/user-roles.enum');
+
+module.exports = {
+  checkIsUserPresent: (req, res, next) => {
+    const { userId } = req.params;
+
+    const userById = userService.findOneById(userId);
+
+    if (!userById) {
+      throw new Error('user not found');
+    }
+    req.user = userById;
+
+    next();
+  },
+  checkIsAdminMiddleware: (req, res, next) => {
+    const { role } = req.user;
+    if (role !== userRolesEnum.ADMIN) {
+      throw new Error('not admin');
+    }
+
+    next();
+  }
+};
